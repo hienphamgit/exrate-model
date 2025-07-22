543cfc765ea249f41558309b32f9da9c3a19e486
Hiên Phạm <hienphambmh@gmail.com>
2025-07-22 18:57:33 +0700

    build model and fix bug

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ with st.spinner("Đang tải dữ liệu..."):
     df = get_data()
 
 with st.sidebar.expander("Dataset"):
-    st.write("Tải lên tập dữ liệu của bạn hoặc sử dụng dữ liệu mẫu.")
+    st.write("Tải lên tập dữ liệu hoặc sử dụng dữ liệu mẫu (Hiên tại chỉ hỗ trợ phân tích dữ liệu theo ngày). Chú ý: Dữ liệu phải có cột 'date' và 'USDVND'.")
     uploaded_file = st.file_uploader("Chọn file CSV", type="csv")
     if uploaded_file is not None:
         try:
@@ -56,7 +56,7 @@ st.sidebar.markdown("### 2. Modelling")
 st.title("Mô hình dự báo tỷ giá hối đoái USD/VND")
 
 st.markdown("## 1. Trực quan hoá dữ liệu")
-st.markdown("Dữ liệu tỷ giá hối đoái USD/VND từ 2019 đến nay.")
+st.markdown("Dữ liệu tỷ giá hối đoái USD/VND từ 2019 đến nay được thu thập từ Yahoo Finance. ")
 # Vẽ biểu đồ đường tỷ giá hối đoái USD/VND
 fig = go.Figure()
 fig.add_trace(go.Scatter(x=df['date'], y=df['USDVND'], mode='lines', name='USD/VND'))
@@ -129,12 +129,13 @@ with col2:
         yaxis=dict(showgrid=True, gridwidth=1, gridcolor='LightGrey')
     )
     st.plotly_chart(fig2, use_container_width=True)
-# Xây dựng mô hình dự báo tỷ giá hối đoái bằng Garch
+
+# --------------- Xây dựng mô hình dự báo tỷ giá hối đoái ----------------------
 st.markdown("## 3. Dự báo tỷ giá hối đoái USD/VND")
 
-col1, col2 = st.columns(2)
+col3, col4 = st.columns(2)
 
-with col1:
+with col3:
     # Widget chọn khoảng thời gian dự báo trong cột 1
     forecast_period = st.slider(
         "Chọn khoảng thời gian dự báo (ngày)",
@@ -142,84 +143,190 @@ with col1:
         max_value=365,
         value=30
     )
-    # Thêm radio button để chọn tìm mô hình hiệu quả nhất hoặc manually chọn mô hình. Nếu chọn manually thì sẽ hiện ra các lựa chọn mô hình GARCH
-    model_type = st.radio(
-        "Chọn mô hình GARCH",
-        options=["Tự động tìm mô hình tốt nhất", "GARCH(1, 1)", "GARCH(1, 2)", "GARCH(2, 1)", "GARCH(2, 2)"],
-        index=0,  # Mặc định chọn "Tự động tìm mô hình tốt nhất"
-        horizontal=True
+    
+    # commbox chọn phân phối cho mô hình GARCH
+    distribution = st.selectbox(
+        "Chọn phân phối cho mô hình GARCH",
+        options=["Normal", "Student's t", "Skewed Student's t"],
+        index=0  # Mặc định chọn Normal
     )
-
-# Xây dựng mô hình GARCH
-if model_type == "Tự động tìm mô hình tốt nhất":
-    # Tự động tìm mô hình GARCH tốt nhất bằng vòng lặp
-    best_aic = np.inf
-    best_model = None
-    for p in range(1, 3):  # Thử p = 1
-        for q in range(1, 3):
-            try:
-                model = arch_model(df['log_return'], vol='Garch', p=p, q=q, dist='t')
-                model_fit = model.fit(disp="off")
-                if model_fit.aic < best_aic:
-                    best_aic = model_fit.aic
-                    best_model = model_fit
-            except Exception as e:
-                st.error(f"Lỗi khi xây dựng mô hình GARCH({p}, {q}): {e}")
-                continue
-    if best_model is not None:
-        st.success(f"Đã tìm thấy mô hình GARCH tốt nhất: {best_model.model.__class__.__name__} với AIC = {best_aic:.2f}")
-        model_fit = best_model    
-else:
-    # Chọn mô hình GARCH theo lựa chọn của người dùng
-    p, q = 1, 1
-    if model_type == "GARCH(1, 2)":
-        p, q = 1, 2
-    elif model_type == "GARCH(2, 1)":
-        p, q = 2, 1
-    elif model_type == "GARCH(2, 2)":
-        p, q = 2, 2
-    model = arch_model(df['log_return'], vol='Garch', p=p, q=q)
-    model_fit = model.fit(disp="off")
-# Hiển thị thông tin mô hình vào cột 2
-with col2:
-    st.markdown("### Thông tin mô hình GARCH")
-    st.write(f"Mô hình: {model_fit.model.__class__.__name__}")
-    st.write(f"AIC: {model_fit.aic:.2f}")
-    st.write(f"BIC: {model_fit.bic:.2f}")
-    st.write(f"Log-Likelihood: {model_fit.loglikelihood:.2f}")
-# Hiển thị tóm tắt mô hình
-col3, col4 = st.columns(2)
-with col3:
-    st.markdown("### Tóm tắt mô hình GARCH")
-    st.write(model_fit.summary())
+    # Chọn loại mô hình ARCH, GARCH, EGARCH, TARCH
+    model_type = st.selectbox(
+        "Chọn loại mô hình ARCH/GARCH",
+        options=["ARCH", "GARCH", "EGARCH", "TARCH"],
+        index=1  # Mặc định chọn GARCH
+    )
 with col4:
-    st.markdown("### Biểu đồ phân tích mô hình GARCH")
-    # Vẽ biểu đồ phân tích mô hình GARCH
-    fig_garch = model_fit.plot(annualize='D')
-    st.pyplot(fig_garch)
+    # Thêm radio button để chọn tìm mô hình hiệu quả nhất hoặc manually chọn mô hình. Nếu chọn manually thì sẽ hiện ra ô nhập giá trị p, q
+    model_selection = st.radio(
+        "Chọn cách xây dựng mô hình",
+        options=["Tự động tìm mô hình tốt nhất", "Tùy chỉnh mô hình"],
+        index=1  # Mặc định chọn Tự động tìm mô hình tốt nhất
+    )
+    # Nếu chọn "Chọn mô hình theo p, q" thì hiển thị ô nhập loại mô hình và giá trị p, q
+    if model_selection == "Tùy chỉnh mô hình":
+        p = st.number_input("Nhập giá trị p (0-3)", min_value=0, max_value=3, value=1)
+        q = st.number_input("Nhập giá trị q (0-3)", min_value=0, max_value=3, value=1)
 
-# Dự báo biến động trong tương lai
-forecast = model_fit.forecast(horizon=forecast_period)
-print(forecast.mean.tail())
-# Hiển thị kết quả tỷ giá hối đoái dự báo
-st.markdown("## 4. Dự báo tỷ giá hối đoái USD/VND ")
-# Tạo DataFrame cho kết quả dự báo
-forecast_df = pd.DataFrame({
-    'date': pd.date_range(start=df['date'].iloc[-1] + pd.Timedelta(days=1), periods=forecast_period, freq='D'),
-    'forecast': forecast.mean.iloc[-forecast_period:].values[0]
-})  
-forecast_df['forecast'] = np.exp(forecast_df['forecast']) * df['USDVND'].iloc[-1]  # Chuyển đổi từ log return về giá trị thực tế
 
-#Chia làm 2 cột để hiển thị dự báo và tỷ giá thực tế
-col5, col6 = st.columns(2)
-with col5:
-    st.dataframe(forecast_df, use_container_width=True)
-with col6:
-    #Cắt bớt dữ liệu df để chỉ lấy 1 năm gần nhất cho biểu đồ
-    df_recent = df[df['date'] >= (df['date'].max() - pd.Timedelta(days=365))]
-    # Hiển thị biểu đồ dự báo tỷ giá hối đoái
-    fig_forecast = go.Figure()
-    fig_forecast.add_trace(go.Scatter(x=df_recent['date'], y=df_recent['USDVND'], mode='lines', name='USD/VND (Thực tế)'))
-    fig_forecast.add_trace(go.Scatter(x=forecast_df['date'], y=forecast_df['forecast'], mode='lines', name='USD/VND (Dự báo)', line=dict(dash='dash')))
-    fig_forecast.update_layout(title='Dự báo tỷ giá hối đoái USD/VND', xaxis_title='Ngày', yaxis_title='Tỷ giá (VND)')
-    st.plotly_chart(fig_forecast, use_container_width=True)
+if model_selection == "Tự động tìm mô hình tốt nhất":
+    # Tự động tìm mô hình tốt nhất
+    with st.spinner("Đang tìm mô hình tốt nhất..."):
+        best_model, best_params = bm.find_best_garch_model(df['log_return'], model_type, distribution, forecast_period)
+        if best_model is None:
+            st.error("Không tìm thấy mô hình GARCH phù hợp.")
+        else:
+            st.success(f"Đã tìm thấy mô hình tốt nhất với tham số (p,q) = {best_params}")
+   
+else:
+    # Tạo mô hình với các tham số tùy chỉnh
+    with st.spinner("Đang xây dựng mô hình với tham số tùy chỉnh..."):
+        if model_type == "ARCH":
+            vol = 'Arch'
+        elif model_type == "GARCH":
+            vol = 'Garch'
+        elif model_type == "EGARCH":
+            vol = 'EGarch'
+        elif model_type == "TARCH":
+            vol = 'Tarch'
+        if distribution == "Normal":
+            dist = 'normal'
+        elif distribution == "Student's t":
+            dist = 't'
+        elif distribution == "Skewed Student's t":
+            dist = 'skewt'
+        try:
+            best_model = arch_model(df['log_return'], vol=vol, p=p, q=q, dist=dist)
+            best_model = best_model.fit(disp='off')
+            st.success(f"Đã xây dựng mô hình với tham số (p,q) = ({p},{q})")
+        except Exception as e:
+            st.error(f"Lỗi khi xây dựng mô hình: {e}")
+            best_model = None
+if best_model is not None:
+    #Hiển thị thông tin mô hình
+    st.write(best_model.summary())
+    #Chia làm 2 cột để hiển thị kết quả dự báo
+    col5, col6 = st.columns(2)
+    with col5:
+            # kiểm định phần dư của mô hình
+        residuals = best_model.resid
+        st.subheader("Kiểm định phần dư của mô hình")
+        fig_residuals = go.Figure()
+        fig_residuals.add_trace(go.Scatter(
+            x=df['date'],
+            y=residuals,
+            mode='lines',
+            name='Phần dư',
+            line=dict(color='blue')
+        ))
+        fig_residuals.update_layout(
+            title='',
+            xaxis_title='',
+            yaxis_title='Phần dư',
+            height=400,
+            margin=dict(l=40, r=20, t=40, b=40),  # Điều chỉnh lề
+            xaxis=dict(showgrid=True, gridwidth=1, gridcolor='LightGrey'),
+            yaxis=dict(showgrid=True, gridwidth=1, gridcolor='LightGrey')
+        )
+        st.plotly_chart(fig_residuals, use_container_width=True)
+    with col6:
+        # Hiển thị phân phối phần dư
+        st.subheader("Phân phối phần dư của mô hình")
+        fig_residuals_dist = go.Figure()
+        fig_residuals_dist.add_trace(go.Histogram(
+            x=residuals,
+            nbinsx=50,
+            name='Phần dư',
+            marker_color='lightblue',
+            opacity=0.8,
+            histnorm='probability density',
+            showlegend=False
+        ))
+        # Tính toán và thêm KDE Trace
+        x_kde = np.linspace(residuals.min(), residuals.max(), 500)
+        kde = gaussian_kde(residuals)
+        kde_y = kde(x_kde)
+        kde_trace = go.Scatter(
+            x=x_kde,
+            y=kde_y,
+            mode='lines',
+            name='KDE',
+            line=dict(color='red', dash='dash', width=2),
+            showlegend=False
+        )
+        fig_residuals_dist.add_trace(kde_trace)
+        fig_residuals_dist.update_layout(
+            xaxis_title='',
+            yaxis_title='',
+            height=400,
+            margin=dict(l=40, r=20, t=40, b=40),  # Điều chỉnh lề
+            xaxis=dict(showgrid=True, gridwidth=1, gridcolor='LightGrey'),
+            yaxis=dict(showgrid=True, gridwidth=1, gridcolor='LightGrey')
+        )
+        st.plotly_chart(fig_residuals_dist, use_container_width=True)
+    if model_type != "EGARCH":
+        # Dự báo với mô hình tốt nhất
+        forecast = best_model.forecast(horizon=forecast_period)
+        conditional_variance_forecast = forecast.variance.iloc[-1]
+        volatility_forecast = np.sqrt(conditional_variance_forecast)
+        st.subheader("Dự báo biến động tỷ giá trong tương lai")
+        # --- 2. Dự báo giá trị (Lợi suất và Tỷ giá) ---
+        # Dự báo lợi suất (mean forecast)
+        mean_forecast = forecast.mean.iloc[-1]
+        # Tính giá trị tỷ giá dự báo
+        # Giá tỷ giá cuối cùng từ dữ liệu gốc
+        last_price = df['USDVND'].iloc[-1]
+        last_date = df["date"].iloc[-1] 
+            # 'B' là tần số ngày làm việc (business day)
+        forecast_dates = pd.date_range(start=last_date, periods=forecast_period + 1, freq='B')[1:]
+
+        # Tạo DataFrame để lưu trữ giá dự báo
+        forecasted_prices = pd.DataFrame(index=forecast_dates) # Gán index ngày tháng ngay lập tức
+        forecasted_prices['Forecasted_USDVND'] = np.nan
+        # thêm cột biến động dự báo
+        forecasted_prices['Forecasted_Volatility'] = np.nan
+        current_forecast_price = last_price
+
+        for i, log_return_pred in enumerate(mean_forecast.values):
+            current_forecast_price = current_forecast_price * np.exp(log_return_pred)
+            forecasted_prices.iloc[i, 1] = volatility_forecast[i]
+            forecasted_prices.iloc[i, 0] = current_forecast_price
+            
+
+        # tạo 2 cột để hiển thị giá trị dự báo và vẽ biểu đồ
+        col7, col8 = st.columns(2)
+
+        with col7:
+            st.write(f"Giá tỷ giá USD/VND dự báo trong {forecast_period} ngày tới:")
+            st.dataframe(forecasted_prices)
+        with col8:
+            st.write("")
+        # cắt bớt dữ liệu df 1 năm gần nhất để vẽ biểu đồ
+        df = df[df['date'] >= (df['date'].max() - pd.DateOffset(years=1))]
+        # Vẽ biểu đồ tỷ giá lịch sử và tỷ giá dự báo với tỷ giá dự báo nét đứt
+        st.subheader("Biểu đồ tỷ giá USD/VND dự báo")
+        fig_forecast = go.Figure()
+        fig_forecast.add_trace(go.Scatter(
+            x=df['date'],
+            y=df['USDVND'],
+            mode='lines',
+            name='Tỷ giá USD/VND lịch sử',
+            line=dict(color='blue')
+        ))
+        fig_forecast.add_trace(go.Scatter(
+            x=forecasted_prices.index,
+            y=forecasted_prices['Forecasted_USDVND'],
+            mode='lines',
+            name='Tỷ giá USD/VND dự báo',
+            line=dict(color='red', dash='dash')
+        ))
+        fig_forecast.update_layout(
+            title='Tỷ giá USD/VND lịch sử và dự báo',
+            xaxis_title='Ngày',
+            yaxis_title='Tỷ giá (VND)',
+            height=400,
+            margin=dict(l=40, r=20, t=40, b=40),  # Điều chỉnh lề
+            xaxis=dict(showgrid=True, gridwidth=1, gridcolor='LightGrey'),
+            yaxis=dict(showgrid=True, gridwidth=1, gridcolor='LightGrey')
+        )
+        st.plotly_chart(fig_forecast, use_container_width=True)
--- buildmodel.py
+++ buildmodel.py
@@ -1,6 +1,44 @@
 import numpy as np
+from arch import arch_model
+import pandas as pd
+
 # --- 2. Tính toán lợi suất Logarithmic ---
 def calculate_log_returns(df):
     df['log_return'] = np.log(df['USDVND'] / df['USDVND'].shift(1))
     df.dropna(inplace=True)  # Loại bỏ giá trị NaN
     return df
+
+def find_best_garch_model(df,model_type, distribution, p_max=3, q_max=3):
+    best_aic = np.inf
+    best_order = None
+    best_model = None
+    
+    if model_type == "ARCH":
+        vol = 'Arch'
+    elif model_type == "GARCH":
+        vol = 'Garch'
+    elif model_type == "EGARCH":
+        vol = 'EGarch'
+    elif model_type == "TARCH":
+        vol = 'Tarch'
+    if distribution == "Normal":
+        dist = 'normal'
+    elif distribution == "Student's t":
+        dist = 't'
+    elif distribution == "Skewed Student's t":
+        dist = 'skewt'
+    # Tìm mô hình GARCH với các giá trị p, q từ 0 đến p_max, q_max
+    for p in range(4):
+        for q in range(4):
+            try:
+                model = arch_model(df, vol=vol, p=p, q=q, dist=dist)
+                model_fit = model.fit(disp='off')
+                if model_fit.aic < best_aic:
+                    best_aic = model_fit.aic
+                    best_order = (p, q)
+                    best_model = model_fit
+            except Exception as e:
+                continue
+
+    return best_model, best_order
+
